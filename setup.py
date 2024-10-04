a0442ba642c0d86d0831a9ea449adee489701085
VijayKanaparthi <githubaccc@gmail.com>
2024-10-04 17:32:01 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-    
+from setuptools import find_packages,setup 
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='vijay',
+    author_email='githubaccc@gmail.com',
+    install_requires=["openai","langchain","streamlit","pyhton-dotenv","pyPDF2"],
+    packages=find_packages()
+)     
